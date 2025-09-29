79d6228a9060fdb79e3ca43434b32f43748ee69e
Roxanne Lutz <rlutz1@unm,edu>
2025-09-29 15:30:49 -0600

    updating back to old version that kinda works....

--- ExamPractice/bf-altered.py
+++ ExamPractice/bf-altered.py
@@ -2,19 +2,18 @@ def bellmanFord(V, edges, src, dest, x):
     
     # Initially distance from source to all other vertices 
     # is not known(Infinite) e.g. 1e8.
-    # dist = [100000000] * V
-    dist = [0] * V
+    dist = [100000000] * V
     dist[src] = 0
 
     # Relaxation of all the edges V times, not (V - 1) as we
     # need one additional relaxation to detect negative cycle
     
-    # for i in range(V):
-    while dist[dest] < x:
+    for i in range(V):
+    # while dist[dest] > x:
       for edge in edges:
           u, v, wt = edge
           # if dist[u] != 100000000 and dist[u] + wt < dist[v]:
-          if dist[u] != 0 and (abs(dist[v] - x) > abs(dist[u] + wt - x)):
+          if dist[u] != 100000000 and (abs(dist[v] - x) > abs(dist[u] + wt - x)):
               # If this is the Vth relaxation, then there 
               # is a negative cycle
               # if i == V - 1:
