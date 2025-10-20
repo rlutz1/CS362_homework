b8e4899d711355ca82572c4a126d9b757e14af41
Roxanne Lutz <rlutz1@unm,edu>
2025-10-20 14:12:05 -0600

    pushing a shitty merging of 2 bsts. note there are no duplicates allowed in these bsts

--- HW2/3_merge_bst.py
+++ HW2/3_merge_bst.py
@@ -8,16 +8,28 @@ class Node:
         self.data = v
 
 # Inorder Traversal
-def printInorder(root):
+def print_inorder(root):
     if root:
         # Traverse left subtree
-        printInorder(root.left)
+        print_inorder(root.left)
         
         # Visit node
         print(root.data,end=" ")
         
         # Traverse right subtree
-        printInorder(root.right)
+        print_inorder(root.right)
+
+# Inorder Traversal
+def print_preorder(root):
+    if root:
+        # Visit node
+        print(root.data,end=" ")
+
+        # Traverse left subtree
+        print_preorder(root.left)
+        
+        # Traverse right subtree
+        print_preorder(root.right)
 
 # Inorder collection of nodes
 def collect_inorder(root, A):
@@ -37,39 +49,93 @@ def is_bst(root):
         return is_bst(root.left) and is_bst(root.right)
     return True
 
-def inorder_merge(root, n, A, i):
-    print(i)
-    if root and i < n:
-      inorder_merge(root.left, n, A, i)
+# def inorder_merge(root, n, A, i):
+#     print(i)
+#     if root and i < n:
+#       inorder_merge(root.left, n, A, i)
+#       if (root and A[i] < root.data):
+#         # insertion of new node
+#         temp = root.left
+#         new_node = Node(A[i])
+#         root.left = new_node
+#         if temp and temp.data > new_node.data:
+#           new_node.right = temp
+#         else:
+#           new_node.left = temp
+#         i += 1
+#         while (i < n and A[i] < root.data):
+#           if not new_node.right:
+#             new_node.right = Node(A[i])
+#             new_node = new_node.right
+#           else:
+#              #something
+#              print("fuckup")
+#           i += 1
+#           # print(i)
+#       i += 1
+#       inorder_merge(root.right, n, A, i)
 
-    
-      if (root and A[i] < root.data):
-        # insertion of new node
-        temp = root.left
-        new_node = Node(A[i])
-        root.left = new_node
-        if temp and temp.data > new_node.data:
-          new_node.right = temp
-        else:
-          new_node.left = temp
+def merge(A, B):
+    n = len(A)
+    m = len(B)
+    i = 0
+    j = 0
+    C = []
+
+    while i < n and j < m:
+        if A[i] < B[j]:
+            C.append(A[i])
+            i += 1
+        elif A[i] > B[j]:
+            C.append(B[j])
+            j += 1
+        else: # they are equal, save one only, assume only distinct in bst
+            C.append(A[i])
+            i += 1
+            j += 1
+
+    while i < n:
+        C.append(A[i])
         i += 1
-        while (i < n and A[i] < root.data):
-          if not new_node.right:
-            new_node.right = Node(A[i])
-            new_node = new_node.right
-          else:
-             #something
-             print("fuckup")
-          i += 1
-          # print(i)
+    
+    while j < m:
+        C.append(B[j])
+        j += 1
+    return C
 
-      i += 1
-          
-      
+def create_BST(sorted_order, n):
+    if n == 0: return None
 
-      inorder_merge(root.right, n, A, i)
+    if n < 3: # special case for now
+        if n == 2:
+            BST_root = Node(sorted_order[0])
+            BST_root.right = Node(sorted_order[1])
+            return BST_root
+        return Node(sorted_order[0])
+    
+    # if larger than 3
+    # make a little subtree
 
-# Driver code
+    BST_root = Node(sorted_order[1])
+    BST_root.left = Node(sorted_order[0])
+    BST_root.right = Node(sorted_order[2])
+
+    subtree = BST_root
+    i = 3
+    while (i < n):
+        BST_root = Node(sorted_order[i])
+        BST_root.left = subtree
+        i += 1
+        if i < n:
+            BST_root.right = Node(sorted_order[i])
+        subtree = BST_root
+        i += 1
+
+    return subtree
+
+
+
+
 
 if __name__ == "__main__":
     # Build the tree
@@ -104,20 +170,34 @@ if __name__ == "__main__":
     # print(is_bst(BST2_root))
 
     # get list of values in order
-    sortedBST = collect_inorder(BST1_root, [])
-    print(sortedBST)
+    sortedBST1 = collect_inorder(BST1_root, [])
+    print(sortedBST1)
 
-    # find correct subtree to append
-    i = 0
-    curr_node = BST2_root
-    while (curr_node and curr_node.right and sortedBST[0] > curr_node.data):
-        curr_node = curr_node.right
+    sortedBST2 = collect_inorder(BST2_root, [])
+    print(sortedBST2)
+
+    mergedOrder = merge(sortedBST1, sortedBST2)
+    print(mergedOrder)
+
+    mergedBST = create_BST(mergedOrder, len(mergedOrder))
+    print("INORDER")
+    print_inorder(mergedBST)
+    print()
+
+    print("PREORDER")
+    print_preorder(mergedBST)
+    print()
+
+    print(is_bst(mergedBST))
+
+    # # find correct subtree to append
+    # i = 0
+    # curr_node = BST2_root
+    # while (curr_node and curr_node.right and sortedBST[0] > curr_node.data):
+    #     curr_node = curr_node.right
     
-    print(curr_node.data)
-     # TODO: go left as well
-    inorder_merge(curr_node, len(sortedBST), sortedBST, 0)
+    # print(curr_node.data)
+    #  # TODO: go left as well
+    # inorder_merge(curr_node, len(sortedBST), sortedBST, 0)
    
-    printInorder(BST2_root)
-
-
-    # This code is contributed by ajaymakvana.
+    # print_inorder(BST2_root)
